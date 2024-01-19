5aafaa97889a5762e804b03b51a2e034555cb390
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-01-19 11:19:05 +0600

    элементы берутся из списка словарей cards вставляются в контейнер HCServiceCards

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -11,6 +11,15 @@ import {useRef} from "react"
 function OurServices() {
   const buttonRef = useRef(null);
 
+  const cards = [
+    {"image": img1, "title": "Search doctor", "subtitle": "Choose your doctor from thousands of specialist, general, and trusted hospitals"},
+    {"image": img2, "title": "Online pharmacy", "subtitle": "Buy your medicines with our mobile application with a simple delivery system"},
+    {"image": img3, "title": "Consultation", "subtitle": "Free consultation with our trusted doctors and get the best recomendations"},
+    {"image": img4, "title": "Details info", "subtitle": "Free consultation with our trusted doctors and get the best recomendations"},
+    {"image": img5, "title": "Emergency care", "subtitle": "You can get 24/7 urgent care for yourself or your children and your lovely family"},
+    {"image": img6, "title": "Tracking", "subtitle": "Track and save your medical history and health data"},
+  ]
+
   function handleClick() {
     buttonRef.current.blur();
   }
@@ -21,12 +30,9 @@ function OurServices() {
       <div className={styles.divider}></div>
       <p className={styles.HCServicesDesc}>We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
       <div className={styles.HCServicesCards}>
-        <ServiceCard image={img1} title="Search doctor" subtitle="Choose your doctor from thousands of specialist, general, and trusted hospitals"/>
-        <ServiceCard image={img2} title="Online pharmacy" subtitle="Buy your medicines with our mobile application with a simple delivery system"/>
-        <ServiceCard image={img3} title="Consultation" subtitle="Free consultation with our trusted doctors and get the best recomendations"/>
-        <ServiceCard image={img4} title="Details info" subtitle="Free consultation with our trusted doctors and get the best recomendations"/>
-        <ServiceCard image={img5} title="Emergency care" subtitle="You can get 24/7 urgent care for yourself or your children and your lovely family"/>
-        <ServiceCard image={img6} title="Tracking" subtitle="Track and save your medical history and health data"/>
+        {cards.map((card) => {
+          return (<ServiceCard image={card["image"]} title={card["title"]} subtitle={card["subtitle"]}/>);
+        })}
       </div>
       <button className={styles.HCServicesButton} onClick={handleClick} ref={buttonRef}>
         Learn more
