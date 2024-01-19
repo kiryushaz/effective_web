0354a1b9d5c32206098802f6c6771ad0987e82b3
Kirill Zykov <kiryushaz.gaming@gmail.com>
2024-01-19 11:13:00 +0600

    в servicecard можно передавать объекты

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -1,5 +1,11 @@
 import ServiceCard from "../ServiceCard/ServiceCard"
 import styles from './OurServices.module.css'
+import img1 from '../../assets/search.svg'
+import img2 from '../../assets/pharmacy.svg'
+import img3 from '../../assets/consultation.svg'
+import img4 from '../../assets/details.svg'
+import img5 from '../../assets/emergency.svg'
+import img6 from '../../assets/tracking.svg'
 import {useRef} from "react"
 
 function OurServices() {
@@ -15,12 +21,12 @@ function OurServices() {
       <div className={styles.divider}></div>
       <p className={styles.HCServicesDesc}>We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
       <div className={styles.HCServicesCards}>
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
+        <ServiceCard image={img1} title="Search doctor" subtitle="Choose your doctor from thousands of specialist, general, and trusted hospitals"/>
+        <ServiceCard image={img2} title="Online pharmacy" subtitle="Buy your medicines with our mobile application with a simple delivery system"/>
+        <ServiceCard image={img3} title="Consultation" subtitle="Free consultation with our trusted doctors and get the best recomendations"/>
+        <ServiceCard image={img4} title="Details info" subtitle="Free consultation with our trusted doctors and get the best recomendations"/>
+        <ServiceCard image={img5} title="Emergency care" subtitle="You can get 24/7 urgent care for yourself or your children and your lovely family"/>
+        <ServiceCard image={img6} title="Tracking" subtitle="Track and save your medical history and health data"/>
       </div>
       <button className={styles.HCServicesButton} onClick={handleClick} ref={buttonRef}>
         Learn more
--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -1,20 +1,22 @@
 import styles from './ServiceCard.module.css'
-import img from '../../assets/search.svg'
+import React from 'react'
 
-function ServiceCard() {
-  return (
-    <>
-      <div className={styles.HCServiceCard}>
-        <div className={styles.HCServiceCardContent}>
-          <div className={styles.HCServiceCardImage}>
-            <img src={img} alt="img" />
+class ServiceCard extends React.Component {
+  render () {
+    return (
+      <>
+        <div className={styles.HCServiceCard}>
+          <div className={styles.HCServiceCardContent}>
+            <div className={styles.HCServiceCardImage}>
+              <img src={this.props.image} alt="img" />
+            </div>
+            <h3 className={styles.HCServiceCardTitle}>{this.props.title}</h3>
+            <p className={styles.HCServiceCardSubTitle}>{this.props.subtitle}</p>
           </div>
-          <h3 className={styles.HCServiceCardTitle}>Search Doctor</h3>
-          <p className={styles.HCServiceCardSubTitle}>Choose your doctor from thousands of specialist, general, and trusted hospitals</p>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default ServiceCard
